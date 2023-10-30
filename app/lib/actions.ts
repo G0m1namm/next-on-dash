f5d6f28666ecc4ebfa1dcba9f7d843e9277182d2
jherson.martelo <jherson.martelo@nexergroup.com>
2023-10-29 23:31:39 -0500

    feat: add chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+import { signIn } from '@/auth';
+
 const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -123,4 +125,18 @@ export async function deleteInvoice(formData: FormData) {
         message: `Database Error: Failed to Deleting Invoice ${id}.`,
       };
     }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
 }
